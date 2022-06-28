14acb45546c40f5e7d75260ca5c681e8b997f054
Zulnabil <zulnabil.22@gmail.com>
2022-06-28 20:50:51 +0700

    feat(init): setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { SES } = require("aws-sdk")
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.listen(6969, () => {
+  console.log("server running")
+})
+
+app.post("/get-ses-quota", (req, res) => {
+  const { key, secret, region } = req.body
+
+  if (!key || !secret || !region)
+    res.json("Please provide key, secret and region!")
+
+  const CONFIG = {
+    accessKeyId: `${key}`,
+    secretAccessKey: `${secret}`,
+    region: `${region}`,
+  }
+
+  try {
+    const ses = new SES(CONFIG)
+    ses.getSendQuota((err, data) => {
+      if (err) res.json(`Error: ${err.message}`)
+
+      res.json(data)
+    })
+  } catch (err) {
+    res.json(`Error: ${err.message}`)
+  }
+})
